a1962d5fa91df014585c045ce7a24114aad3d7a9
Druvakumar0441 <nani.rayini@gmail.com>
2024-01-03 18:36:33 +0530

    new files

---
+++ src/com/gitrepository/StringForEach.java
@@ -0,0 +1,11 @@
+package com.gitrepository;
+
+public class StringForEach {
+
+	public static void main(String[] args) {
+		String[] s= {"Druva", "kumar","hello", "Java"};
+		for (String string : s) {
+			System.out.println(string);
+		}
+	}
+}
